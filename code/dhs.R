19bea11f6cd04ea7e0652b3f43c2bdaeb303bcd7
Kinh Nguyen <4459378+kklot@users.noreply.github.com>
2022-01-20 02:20:11 +0100

    format diagram

--- code/dhs.R
+++ code/dhs.R
@@ -261,25 +261,25 @@ dta %>%
 #'
 #+ aamModel, fig.cap = "AAM risk set model", fig.height=3
 bind_rows(
-    tibble(start = char(birth, afs, aam), end = char(afs, aam, aai), a = 0:2, z = 1:3, set = 1, risk = c(0, 1, 0)),
-    tibble(start = char(birth, afs), end = char(afs, aai), a = c(0, 1), z = c(1, 3), set = 2, risk = c(0, 1)),
-    tibble(start = char(birth, aam, afs), end = char(aam, afs, aai), a = c(0, .7, 1), z = c(.7, 1, 3), set = 3, risk = c(1, 0, 0)),
-    tibble(start = char(birth, aam), end = char(aam, aai), a = c(0, .7), z = c(.7, 3), set = 4, risk = c(1, 0)),
+    tibble(start = char(birth, afs, aam), end = char(afs, aam, aai), a = 0:2, z = 1:3, risk = c(0, 1, 0)),
+    tibble(start = char(birth, afs), end = char(afs, aai), a = c(0, 1), z = c(1, 3), risk = c(0, 1)),
+    tibble(start = char(birth, aam, afs), end = char(aam, afs, aai), a = c(0, .7, 1), z = c(.7, 1, 3), risk = c(1, 0, 0)),
+    tibble(start = char(birth, aam), end = char(aam, aai), a = c(0, .7), z = c(.7, 3), risk = c(1, 0)),
     #' no AFS
-    tibble(start = char(birth), end = char(aai), a = c(0), z = c(3), set = 5, risk = c(1))
+    tibble(start = char(birth), end = char(aai), a = c(0), z = c(3), risk = c(1)),
+    .id = 'set'
 ) %>%
     mutate(risk = factor(risk, labels = char(No, Yes)), 
-    across(where(is.character), str_to_upper)) %>%s
-        geom_segment(aes(a, 1, xend = z, yend = 1, color=risk), 
-        arrow = arrow(type = "closed")) +
+    across(where(is.character), str_to_upper)) %>%
+    ggplot() +
+        geom_segment(aes(a, 1, xend = z, yend = 1, color=risk), arrow = arrow(type = "closed")) +
         geom_text(aes(a, 1.1, label = start)) +
-            geom_text(aes(z, 1.1, label = end)) +
-            theme_void() +
-            scale_color_manual(values = c("grey", "#FD7C02")) +
-            coord_cartesian(ylim = c(.5, 1.5)) +
-            facet_grid(vars(set)) +
-            guides(color = "none")
-
+        geom_text(aes(z, 1.1, label = end)) +
+        theme_void() +
+        scale_color_manual(values = c("grey", "#FD7C02")) +
+        coord_cartesian(ylim = c(.5, 1.5)) +
+        facet_grid(vars(set)) +
+        guides(color = 'none')
 #' The first case is the typical states transition with observed marriage event;
 #' the second case is right censored at the time of interview. The third and
 #' fourth case are special in that the time "at risk of getting marriage" are
