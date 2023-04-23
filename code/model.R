e91e29d27901111f25dcbaa968e58dac48076ee3
Kinh Nguyen <nguyenkinh@ytecongcong.com>
2023-04-23 14:18:16 +0200

    gompertz

--- code/model.R
+++ code/model.R
@@ -11,46 +11,88 @@ invisible(TMB::config(tape.parallel = 0, DLL = "model"))
 TMB::openmp(1)
 
 data <- readRDS(here("data/prep.rds")) %>%
+d <- readRDS(here("data/d.rds"))
+N <- 3000
+data <- d %>% 
     mutate(across(everything(), haven::zap_label)) %>%
     mutate(across(everything(), as.integer)) %>%
     slice_sample(n = 100) %>%
+    mutate(J = J - 1) %>%
+    slice_head(n = N) %>% 
     as.list()
 
+data$modelmatrix <- model.matrix(~ age , data.frame(age = 0:MAX_AGE))
+
 expect_true(max(data$age) <= MAX_AGE)
+expect_true(max(data$delta) <= MAX_AGE)
 expect_true(all((data$afs + data$n_N) <= data$age))
 expect_true(all((data$aam + data$n_N) <= data$age))
+expect_true(max(data$J) == 6)
 
 data$sd_b <- c(0, 0.1)
 data$sd_q <- c(log(0.001), 0.1) # log normal mean and sd
 str(data)
 
-init <- list(
-    betav = rnorm((MAX_AGE + 1) * N_PAR, 0, 0.1),
-    lqv = rnorm(N_PAR, log(0.001), 1),
-    pacf = c(0, 1)
-)
+init <- list(betas = rnorm(2 * N_PAR, log(0.001), 1))
 str(init)
 
 obj <- TMB::MakeADFun(data, init, DLL = "model", silent = TRUE)
-
-fit <- nlminb(obj$par, obj$fn, obj$gr, control = list(trace = 0))
-rp <- obj$report(obj$env$last.par.best)
-str(rp)
-
+fit <- nlminb(obj$par, obj$fn, obj$gr, control = list(trace = 1, maxit = 500))
+rp <- obj$report(obj$env$last.par.best); str(rp)
 p_id <- char(VX, XM, MS, MD, MW, SR, DR, WR)
 p_lb <- char(
     "Sexual debut", "Marriage", "Separate", "Divorce", "Widow",
     "Separated>>Remarried", "Divorced>>Remarried", "Widowed>>Remarried"
 )
 
-rp$`KM.est` %>%
-    as_tibble() %>%
-    rename_with(~p_lb) %>%
-    rownames_to_column("age") %>%
-    mutate(age = as.numeric(age)) %>%
-    pivot_longer(-age) %>%
-    mutate(name = factor(name, levels = p_lb)) %>%
-    ggplot(aes(age, value, color = name)) +
-    geom_line() +
-    scale_color_manual(values = thematic::okabe_ito())
+vis <- function(x, rt = F) {
+    X <- array(rp[[x]], c(7, 7, 50))
+    tibble(
+        age = 1:50,
+        VX = X[1, 2, ],
+        VM = X[1, 3, ],
+        XM = X[2, 3, ],
+        MS = X[3, 4, ],
+        MD = X[3, 5, ],
+        MW = X[3, 6, ],
+        SR = X[4, 7, ],
+        DR = X[5, 7, ],
+        WR = X[6, 7, ],
+    ) %>%
+        pivot_longer(-age) %>%
+        mutate(name = factor(name, levels = char(VX, VM, XM, MS, MD, MW, SR, DR, WR))) %>%
+        allot(o)
+    if (rt) return(o)
+    o %>% ggplot(aes(age, value, color = name)) +
+        geom_line()
+}
 
+vis("KM.masterP") + facet_wrap(~name, scales = "free")
+
+vis("KM.masterP", 1) %>% 
+pivot_wider(names_from = name, values_from = value) %>% 
+transmute(
+    age = age,
+    pX = VX,
+    pM = (VX * XM) + VM, 
+    pNULL = NA,
+    pS = pM * MS,
+    pD = pM * MD,
+    pW = pM * MW,
+    pSr = pS * SR,
+    pDr = pD * DR,
+    pWr = pW * WR,
+    ) %>% 
+    pivot_longer(-age) %>% 
+    mutate(name = factor(name, 
+        levels = char(pX, pM, pNULL, pS, pD, pW, pSr, pDr, pWr), 
+        labels = char("Debut", "Marriage", "", "Separated|Married", "Divorced|Married", "Widowed|Married", "Remarried|Separated", "Remarried|Divorced", "Remarried|Widowed"), 
+        )) %>% 
+    ggplot(aes(age, value, color = name)) + geom_line() + facet_wrap(~name, scales = 'free') +
+    scale_y_continuous(labels = scales::percent, breaks = scales::pretty_breaks(n = 7)) +
+    theme(
+        legend.position = 'none',
+        panel.grid.minor = element_blank(), panel.grid.major.x = element_blank(), axis.text.y = element_text(size = 5)) +
+    labs(title = "Transition probability in the next year", y = '')
+
+ggsave("fig/prob3000.png", width = 7, height = 4.5)
--- code/model.cpp
+++ code/model.cpp
@@ -2,48 +2,56 @@
 
 #define AGE_MAX 50
 #define N_AGE 51
+#define N_D 51 // differences, more than needed
 #define N_PAR 8
 #define N_Q 7
 
 template <class T> 
 struct Kube {
-    vector<T> masterQ;
-    vector<T> masterP;
-    matrix<T> qM;
-    matrix<T> pM;
-    matrix<T> est;
-    int len;
+    vector<T> 
+        masterQ = vector<T>(N_AGE * N_Q * N_Q),
+        masterP = vector<T>(N_AGE * N_Q * N_Q),
+        masterD = vector<T>(N_AGE * N_Q * N_Q * N_D);
+    matrix<T> 
+        qM = matrix<T>(N_Q, N_Q),
+        pM = matrix<T>(N_Q, N_Q), 
+        pD = matrix<T>(N_Q, N_Q), 
+        est = matrix<T>(N_AGE, N_PAR);
+    int len = N_Q * N_Q;
     Kube () {};
-    Kube (vector<T> betav, vector<T> qv) : 
-        masterQ(N_AGE * N_Q * N_Q), 
-        masterP(N_AGE * N_Q * N_Q), 
-        qM(N_Q, N_Q), 
-        pM(N_Q, N_Q), 
-        est(N_AGE, N_PAR),
-        len(N_Q * N_Q)
+    Kube (matrix<T> modelmatrix, vector<T> betas)
     {
         masterP.setZero();
         masterQ.setZero();
-        betav = exp(betav);
-        matrix<T> betam = betav.reshaped(N_AGE, N_PAR);
-        betam.array().rowwise() *= qv.transpose(); // *= only array
-        est = betam; // save for reporting
+        masterD.setZero();
+        for (int i = 0; i < N_PAR; i++)
+            est.col(i) = modelmatrix * betas({i, i + N_PAR}).matrix();
+        est = exp(est.array());
         for (int i = 0; i < N_AGE; i++) {
             qM.setZero(); 
-            qM(0, 1) = betam(i, 0); // debut
-            qM(1, 2) = betam(i, 1); // marriage
-            qM(2, {3,4,5}) = betam(i, {2,3,4}); // marriage dissolution
-            qM({3,4,5}, 6) = betam(i, {5,6,7}); // disso > remarried
-            qM(6, {3,4,5}) = betam(i, {2,3,4}); // remarried > disso = married > disso, we could add a(three) scaling parameter as well?
+            qM(0, 1) = est(i, 0); // debut
+            qM(1, 2) = est(i, 1); // marriage
+            qM(2, {3,4,5}) = est(i, {2,3,4}); // marriage dissolution
+            qM({3,4,5}, 6) = est(i, {5,6,7}); // disso > remarried
+            qM(6, {3,4,5}) = est(i, {2,3,4}); // remarried > disso = married > disso, we could add a(three) scaling parameter as well?
             qM.diagonal() = T(-1) * qM.rowwise().sum();
             memcpy(&masterQ(0) + i * len, &qM(0), sizeof(T) * len);
             pM = expm(qM);
             memcpy(&masterP(0) + i * len, &pM(0), sizeof(T) * len);
+            for (int j = 0; j < N_D; j++) { // prep delta
+                matrix<T> tmp = qM * j;
+                pD = expm(tmp);
+                memcpy(&masterD(0) + i * len * N_D + j * len, &pD(0), sizeof(T) * len);
+            }
         }
     };
     matrix<T> operator()(int a, bool diag = true){
         matrix<T> ans = masterP(Eigen::seqN(a * len, len)).reshaped(N_Q, N_Q);
         if (!diag) for (int i = 0; i < N_Q; ++i) ans(i, i) = T(0);
+        return ans;
+    };
+    matrix<T> operator()(int a, int d, bool diag = true){
+        matrix<T> ans = masterD(Eigen::seqN(a * len * N_D + d * len, len)).reshaped(N_Q, N_Q);
         return ans;
     }
 };
@@ -87,134 +95,59 @@ Type objective_function<Type>::operator() ()
   DATA_IVECTOR(age);
   DATA_IVECTOR(aam);
 
-  DATA_IVECTOR(u);
-  DATA_IVECTOR(ms);
-  DATA_IVECTOR(n_X);
-  DATA_IVECTOR(n_M);
-  DATA_IVECTOR(n_N);
+  DATA_IVECTOR(VV);
+  DATA_IVECTOR(VX);
+  DATA_IVECTOR(XX);
+  DATA_IVECTOR(XM);
+  DATA_IVECTOR(MM);
+  DATA_IVECTOR(MJ);
+  DATA_IVECTOR(J);
+  DATA_IVECTOR(delta);
+  
+  DATA_MATRIX(modelmatrix);
 
   // priors
   DATA_VECTOR(sd_b);
   DATA_VECTOR(sd_q);
 
-  // AR2 of age
-  PARAMETER_VECTOR(betav);
-  PARAMETER_VECTOR(pacf);
-  mAR<Type> MAR2(2);
-  for (int i = 0; i < N_PAR; i++)
-    prior += MAR2(pacf, betav(Eigen::seqN(i * N_AGE, N_AGE)));
+  // Coefs
+  PARAMETER_VECTOR(betas); 
+  prior -= dnorm(betas, sd_q(0), sd_q(1), true).sum();
+  // Soft-constraints remarried to be the same
+  prior -= dnorm(betas(5) - betas(6), Type(0), Type(0.001), true);
+  prior -= dnorm(betas(5) - betas(7), Type(0), Type(0.001), true);
+  prior -= dnorm(betas(N_PAR + 5) - betas(N_PAR + 6), Type(0), Type(0.001), true);
+  prior -= dnorm(betas(N_PAR + 5) - betas(N_PAR + 7), Type(0), Type(0.001), true);
+  Kube<Type> KM(modelmatrix, betas);
 
-  // Baseline intensity
-  PARAMETER_VECTOR(lqv); 
-  prior -= dnorm(lqv, sd_q(0), sd_q(1), true).sum() + sum(lqv);
-  vector<Type> qv = exp(lqv);
-
-  Kube<Type> KM(betav, qv);
-
-  vector<Type> o(afs.size());
+  matrix<Type> o(afs.size(), 6);
   o.setOnes();
 
   for (int i = 0; i < afs.size(); i++) {
-    if (afs[i] == 0) {
-        for (int a = 0; a < age[i]; a++) 
-            o[i] *= KM(a)(0, 0);
-    } else {
-        for (int a = 0; a < afs[i]; a++) 
-            o[i] *= KM(a)(0, 0);
-        o[i] *= KM(afs[i])(0, 1);
-        if (n_X[i] == 0 & aam[i] == 0) 
-            o[i] *= KM(afs[i])(1, 1);
-        if (n_X[i] > 0) 
-            for (int a = afs[i]; a < (afs[i] + n_X[i]); a++)
-                o[i] *= KM(a)(1, 1); // XX(afs:(afs+n_X))
-        if (aam[i] > 0) {
-            o[i] *= KM(aam[i])(1, 2); // XM(aam)
-            if (u[i] == 1) {
-                if (n_M[i] == 0) 
-                    o[i] *= KM(aam[i])(2, 2); // MM(aam)
-                if (n_M[i] > 0)
-                    for (int a = aam[i]; a < age[i]; a++) 
-                        o[i] *= KM(a)(2, 2); // MM(aam:age)
-                if (n_M[i] == -2908) {
-                    int J = ms[i] - 1;
-                    if (n_N[i] == 0) 
-                        o[i] *= KM(aam[i])(2, J); // MJ(aam)
-                    if (n_N[i] > 0) {
-                        matrix<Type> oo(2, 2); oo.setOnes();
-                        if (n_N[i] > 1)
-                            for (int a = 1; a < n_N[i] - 1; a++)
-                                oo *= KM(aam[i] + a)({2, J}, {2, J});
-                        o[i] *= KM(aam[i])(2, {2, J}) * oo * 
-                                KM(aam[i] + n_N[i])({2, J}, J);
-                    }
-                }
-            }
-            if (u[i] > 1) {
-                int J = ms[i] - 1;
-                if (n_N[i] == 3) { // M...J(aam:age+3)
-                    o[i] *= (KM(aam[i] + 0)(2, {3,4,5}) *
-                            KM(aam[i] + 1)({3,4,5}, 6) *
-                            KM(aam[i] + 2)(6, J)).value(); // 77 = RM = RR =? MM
-                }
-                if (n_N[i] == 4) { // M...J(aam:age+4)
-                    if (ms[i] != 7)
-                        o[i] *= (KM(aam[i] + 0)(2, {2,3,4,5}) *
-                                KM(aam[i] + 1)({2,3,4,5}, {3,4,5,6}) *
-                                KM(aam[i] + 2, false)({3,4,5,6}, {J, 6}) *
-                                KM(aam[i] + 3)({J, 6}, J)).value();
-                    if (ms[i] == 7)
-                        o[i] *= (KM(aam[i] + 0)(2, {2,3,4,5}) *
-                                KM(aam[i] + 1)({2,3,4,5}, {3,4,5,6}) *
-                                KM(aam[i] + 2)({3,4,5,6}, {3,4,5,6}) *
-                                KM(aam[i] + 3)({3,4,5,6}, 6)).value();
-                }
-                if (n_N[i] > 4) { // M...J(aam:age+5+)"
-                    matrix<Type> oo(5, 5); oo.setOnes();
-                    matrix<Type> qp(2, 2); oo.setOnes();
-                    if (n_N[i] > 5) {
-                        for (int a = 2; a < (n_N[i] - 4); a++) {
-                            oo *= KM(aam[i] + a)({2,3,4,5,6}, {2,3,4,5,6});
-                            if (J != 7)
-                                qp *= KM(aam[i] + a)({2,J}, {2,J});
-                        }
-                    }
-                    if (ms[i] != 7)
-                        o[i] *=
-                        (
-                            KM(aam[i] + 0)(2, {2,3,4,5}) *
-                            KM(aam[i] + 1)({2,3,4,5}, {2,3,4,5,6}) *
-                            oo *
-                            KM(aam[i] + n_N[i] - 3)({2,3,4,5,6}, {3,4,5,6}) *
-                            KM(aam[i] + n_N[i] - 2)({3,4,5,6}, {J, 6}) *
-                            KM(aam[i] + n_N[i] - 1)({J, 6}, J) -
-                            // substract one union cases
-                            KM(aam[i] + 0)(2, {2, J}) *
-                            KM(aam[i] + 1)({2, J}, {2, J}) *
-                            qp *
-                            KM(aam[i] + n_N[i] - 3)({2, J}, J) *
-                            KM(aam[i] + n_N[i] - 2)(J, J) *
-                            KM(aam[i] + n_N[i] - 1)(J, J)
-                        ).value();
-                    if (ms[i] == 7)
-                        o[i] *=
-                        (
-                            KM(aam[i] + 0)(2, {2,3,4,5}) *
-                            KM(aam[i] + 1)({2,3,4,5}, {2,3,4,5,6}) *
-                            oo *
-                            KM(aam[i] + 2)({2,3,4,5,6}, {3,4,5,6}) *
-                            KM(aam[i] + 3)({3,4,5,6}, {3,4,5,6}) *
-                            KM(aam[i] + 4)({3,4,5,6}, J)
-                        ).value();
-                }
-            }
-        }
-    }
+    if (VV[i] > 0)
+        for (int j = 0; j <= VV[i]; j++)
+            o(i, 0) *= KM(j)(0,0);
+    if (VX[i] > 0)
+            o(i, 1) *= KM(VX[i])(0,1);
+    if (XX[i] > 0)
+        for (int j = afs[i]; j <= afs[i] + XX[i]; j++) 
+            o(i, 2) *= KM(j)(1,1);
+    if (XM[i] > 0)
+            o(i, 3) *= KM(XM[i])(1,2);
+    if (MM[i] > 0)
+        for (int j = aam[i]; j <= aam[i] + MM[i]; j++) 
+            o(i, 4) *= KM(j)(2,2);
+    if (MJ[i] > 0)
+            o(i, 5) *= KM(MJ[i], delta[i])(2,J[i]);
   }
-  dll += prior - sum(log(o));
-  REPORT(betav);
-  REPORT(qv);
+  REPORT(o);
+  o = log(o.array());
+  dll += prior - o.sum();
+
+  REPORT(betas);
   REPORT(KM.est);
   REPORT(KM.masterP);
   REPORT(KM.masterQ);
+  REPORT(KM.masterD);
   return dll;
 }
